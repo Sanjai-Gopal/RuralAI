02c8b28ae4acae8518e2f95ddeeb83e304fd501d
Sanjai-Gopal <sanjai23062008g@gmail.com>
2026-02-18 00:23:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ import re
 from collections import defaultdict
 from datetime import datetime
 
-app = Flask(__name__)
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(
+    __name__,
+    static_folder=os.path.join(BASE_DIR, "static"),
+    template_folder=os.path.join(BASE_DIR, "templates")
+)
+
 
 # ==============================
 # In-Memory Storage (Hackathon Safe)
